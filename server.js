bf50c16c8c92e960aebb9e91dd024b8824051785
AlessoMTZ <gastonmetzgera@gmail.com>
2025-03-25 14:15:03 -0500

    (update): clean comments in server.js

--- server.js
+++ server.js
@@ -1,18 +1,12 @@
-<<<<<<< HEAD
 require("dotenv").config();
-=======
 require ("dotenv").config();
->>>>>>> 5d03e639eadf70ab7959bc0e748cdbb0a58e4021
+
 const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 5d03e639eadf70ab7959bc0e748cdbb0a58e4021
 const app = express();
 
 //Middlewares
@@ -21,7 +15,7 @@ app.use(helmet());
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 
-<<<<<<< HEAD
+
 const employeeRoutes = require("./routes/employeeRoutes");
 const inventoryRoutes = require("./routes/inventoryRoutes");
 const menuRoutes = require("./routes/menuRoutes");
@@ -33,11 +27,11 @@ app.use("/inventory", inventoryRoutes);
 app.use("/menu", menuRoutes);
 app.use("/orders", orderRoutes);
 app.use("/sales", salesRoutes);
-=======
+
 // app.use("/api/usuarios", usuarioRoutes);
 // app.use("/api/auth", authRoutes);
 // app.use("api/pedidos", pedidoRoutes);
->>>>>>> 5d03e639eadf70ab7959bc0e748cdbb0a58e4021
+
 
 const PORT = process.env.DB_PORT || 3000;
 
