75e6c9213ee35732b9f61f5659906bb22d61d105
Perrony203 <perrony203@gmail.com>
2025-03-25 13:30:54 -0500

    fix: server

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
+require ("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+//Middlewares
 app.use(cors());
 app.use(helmet());
 app.use(morgan("dev"));
@@ -5,6 +15,7 @@ app.use(bodyParser.json());
 
 // app.use("/api/usuarios", usuarioRoutes);
 // app.use("/api/auth", authRoutes);
+// app.use("api/pedidos", pedidoRoutes);
 
 const PORT = process.env.DB_PORT || 3000;
 
